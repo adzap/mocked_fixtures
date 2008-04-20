a0909a55366f933446113371d82ff41bf461651f
ameehan <ameehan@4b8e6711-387e-ad41-99ec-b0ebad7a67e0>
2008-04-20 10:24:56 +0000

    set default schema path
    git-svn-id: https://svn.researchpath.com.au/rails/plugins/mocked_fixtures/trunk@736 4b8e6711-387e-ad41-99ec-b0ebad7a67e0

--- lib/mocked_fixtures.rb
+++ lib/mocked_fixtures.rb
@@ -8,3 +8,5 @@ require 'spec/rails' rescue nil
 if defined?(Spec::Rails::Example::RailsExampleGroup)
   Spec::Rails::Example::RailsExampleGroup.send(:include, MockedFixtures::MockExtensions)
 end
+
+MockedFixtures::SchemaParser.schema_path = RAILS_ROOT + '/db/schema.rb'
