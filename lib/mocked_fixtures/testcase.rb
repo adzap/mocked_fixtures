d8933a09328e649522be4074d42d1411f538f685
Adam Meehan <adam.meehan@gmail.com>
2008-07-25 10:44:44 +1000

    added fixture accessors examples

--- lib/mocked_fixtures/testcase.rb
+++ lib/mocked_fixtures/testcase.rb
@@ -8,15 +8,23 @@ module Test
       superclass_delegating_accessor :loaded_mock_fixtures
       superclass_delegating_accessor :mock_fixtures_loaded
       superclass_delegating_accessor :global_mock_fixtures
-      superclass_delegating_accessor :mocked_fixtures_mock_with
+      superclass_delegating_accessor :mocked_fixtures_mock_framework
 
       self.mock_fixture_table_names = []
       self.loaded_mock_fixtures = {}
       self.mock_fixtures_loaded = false
-      
+     
       def self.global_mock_fixtures=(*table_names)
         table_names = self.all_fixture_table_names if table_names.first == :all
         self.mock_fixtures table_names.flatten.map { |n| n.to_s }
+      end
+      
+      def self.mocked_fixtures_mock_with=(mock_framework)
+        require "mocked_fixtures/mocks/#{mock_framework}"
+        self.mocked_fixtures_mock_framework = mock_framework
+        unless mock_framework == :rspec
+          self.class.send(:alias_method, :mock_model, "mock_model_with_#{mock_framework}".to_sym) 
+        end
       end
       
       def self.mock_fixtures(*table_names)
@@ -34,13 +42,6 @@ module Test
         table_names = Dir["#{fixture_path}/*.yml"] + Dir["#{fixture_path}/*.csv"]
         table_names.map { |f| File.basename(f).split('.')[0..-2].join('.') }
       end
-      
-      def setup_with_mock_fixtures
-        fixtures_to_load = self.class.mock_fixture_table_names - self.class.loaded_mock_fixtures.keys
-        return if fixtures_to_load.empty?
-        load_mock_fixtures(fixtures_to_load)
-      end
-      alias_method :setup, :setup_with_mock_fixtures
       
       # Adds test case setup method to run before each test to load fixture
       # files and setup mock fixture accessors for test class. This runs before
@@ -63,9 +64,9 @@ module Test
         @__disable_method_added__ = false
         method_added_without_mock_fixtures(method)
       end
-      
+     
       class << self
-        alias_method_chain :method_added, :mock_fixtures
+        alias_method_chain :method_added, :mock_fixtures        
       end
      
       # This creates the fixture accessors and retrieves the fixture and creates
@@ -84,18 +85,27 @@ module Test
             instances = fixtures.map do |fixture_name|
               if fixture = self.class.loaded_mock_fixtures[table_name][fixture_name.to_s]
                 model_class    = self.class.loaded_mock_fixtures[table_name].send(:model_class)
-                mock_type      = self.class.mocked_fixtures_mock_with
-                # create mock and cache
+                mock_type      = self.class.mocked_fixtures_mock_framework
+
                 @mock_fixture_cache[table_name][fixture_name] ||= MockedFixtures::MockFactory.create_mock(mock_type, model_class, fixture, self)
               else
                 raise StandardError, "No mock fixture with name '#{fixture}' found for table '#{table_name}'"
               end
             end
+
             instances.size == 1 ? instances.first : instances
           end
+          
         end
       end
       
+      def setup_with_mock_fixtures
+        fixtures_to_load = self.class.mock_fixture_table_names - self.class.loaded_mock_fixtures.keys
+        return if fixtures_to_load.empty?
+        load_mock_fixtures(fixtures_to_load)
+      end
+      alias_method :setup, :setup_with_mock_fixtures
+
       # Loads fixtures to be mocked and cache them in class variable
       def load_mock_fixtures(fixtures_to_load)
         fixtures = MockedFixtures::MockFixtures.create_fixtures(fixture_path, fixtures_to_load, fixture_class_names)
