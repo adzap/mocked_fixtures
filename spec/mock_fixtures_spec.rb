18fa305edb0ef71c0881dd18bac4a347d3e8bde0
Adam Meehan <adam.meehan@gmail.com>
2008-07-25 11:02:24 +1000

    removed mocking specs into mock_factory specs

--- spec/mock_fixtures_spec.rb
+++ spec/mock_fixtures_spec.rb
@@ -8,7 +8,12 @@ describe MockedFixtures::MockFixtures do
   end
   
   before(:each) do
-    @fixture_path = File.expand_path(File.dirname(__FILE__) + '/resources')    
+    @fixture_path = Test::Unit::TestCase.fixture_path
+  end  
+
+  it "should return primary key for fixture table" do
+    fixtures = MockedFixtures::MockFixtures.create_fixtures(@fixture_path, [:companies])
+    fixtures[0].primary_key_name.should == 'cid'
   end  
   
   it "should create fixtures" do
@@ -21,30 +26,5 @@ describe MockedFixtures::MockFixtures do
     fixtures = MockedFixtures::MockFixtures.create_fixtures(@fixture_path, [:employees])
     fixtures.first['adam'][:company_id].should == Fixtures.identify('mega_corp')
   end
-
-  it "should return primary key for fixture table" do
-    fixtures = MockedFixtures::MockFixtures.create_fixtures(@fixture_path, [:companies])
-    fixtures[0].primary_key_name.should == 'cid'
-  end  
   
-  describe "mocked objects" do
-    mock_fixtures :companies, :employees
-    
-    it "should return values for column methods from fixture" do
-      mock_companies(:mega_corp).name.should == 'Mega Corporation'
-    end
-    
-    it "should have attributes not in fixture stubbed to return nil" do
-      mock_companies(:mega_corp).should respond_to(:address)
-      mock_companies(:mega_corp).address.should be_nil
-    end
-    
-    it "should return all named fixtures as array" do
-      mock_employees(:adam, :jane).size.should == 2
-    end
-    
-    it "should return all fixtures when passed :all option" do
-      mock_employees(:all).size.should == 2
-    end
-  end  
 end
