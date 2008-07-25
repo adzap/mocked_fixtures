b5a36d20b376540f283974db5058be081eff2fac
Adam Meehan <adam.meehan@gmail.com>
2008-07-25 11:02:54 +1000

    removed other mocking library loading for the meantime until it works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,16 +9,17 @@ require 'active_record/fixtures'
 require 'active_support'
 
 require 'rspec_on_rails/rspec_on_rails'
-require 'flexmock/test_unit'
-require 'mocha'
+#require 'flexmock/test_unit'
+#require 'mocha'
 
 require 'mocked_fixtures/schema_parser'
 require 'mocked_fixtures/testcase'
 require 'mocked_fixtures/mock_fixtures'
 require 'mocked_fixtures/mock_factory'
+require 'mocked_fixtures/mock_connection'
 require 'mocked_fixtures/mocks/rspec'
-require 'mocked_fixtures/mocks/flexmock'
-require 'mocked_fixtures/mocks/mocha'
+#require 'mocked_fixtures/mocks/flexmock'
+#require 'mocked_fixtures/mocks/mocha'
 
 require 'resources/company'
 require 'resources/employee'
@@ -28,4 +29,4 @@ MockedFixtures::SchemaParser.schema_path = File.expand_path(File.dirname(__FILE_
 
 Test::Unit::TestCase.fixture_path = File.expand_path(File.dirname(__FILE__) + '/resources')
 
-Test::Unit::TestCase.mocked_fixtures_mock_with = :rspec
+
