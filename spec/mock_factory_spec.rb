314b31f1cadb199eca23aa2901afc1e95f18c1c7
Adam Meehan <adam.meehan@gmail.com>
2008-07-25 10:28:33 +1000

    commented out flexmock and mocha testing for now added more specs for errros stub

--- spec/mock_factory_spec.rb
+++ spec/mock_factory_spec.rb
@@ -1,31 +1,63 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe MockedFixtures::MockFactory do
-  def klass; MockedFixtures::MockFactory; end
-
   before do
-    @fixture = { :cid => 6, :name => 'Hot Mocks'}
+    @fixture = {:id => 6, :cid => 6, :name => 'Hot Mocks'}
   end
-  
-  [:rspec, :flexmock, :mocha].each do |type|
-    describe "#{type.to_s.titleize} integration" do
+
+  #[:rspec, :flexmock, :mocha].each do |type|
+  [:rspec].each do |type|
+    require "mocked_fixtures/mocks/#{type}"
+    require Spec::Runner.configuration.mock_framework
+    include Spec::Plugins::MockFramework
+    
+    describe "#{type.to_s.titleize} integration" do      
+      attr_accessor :the_mock
       
-      it "should create mock fixture object" do  
-        mock = klass.create_mock(type, Company, @fixture, self)
-        mock.should_not be_nil
+      before do
+        @the_mock = MockedFixtures::MockFactory.create_mock(type, Company, @fixture, self)        
       end
       
-      it "should create mock object with all attributes" do  
-        mock = klass.create_mock(type, Company, @fixture, self)
-        mock.should respond_to(:cid, :name, :address, :created_at, :updated_at)
+      it "should create mock fixture object" do        
+        the_mock.should_not be_nil
       end
       
-      it "should create mock object with attributes value from fixture" do  
-        mock = klass.create_mock(type, Company, @fixture, self)
-        mock.cid.should == 6
-        mock.name.should == 'Hot Mocks'
+      it "should create mock object with all attributes" do        
+        the_mock.should respond_to(:cid, :name, :address, :created_at, :updated_at)
+      end
+      
+      it "should create mock object with attributes value from fixture" do
+        the_mock.cid.should == 6
+        the_mock.name.should == 'Hot Mocks'
+      end
+      
+      it "should create mock with id stubbed to fixture" do
+        the_mock.id.should == 6
+      end
+      
+      it "should create mock with new_record? stubbed to false" do
+        the_mock.new_record?.should be_false        
+      end
+      
+      it "should create mock with to_param stubbed to id as string" do
+        puts "#{type} " + the_mock.to_param
+        the_mock.to_param.should == "6"
+      end
+      
+      it "should create mock with errors stubbed" do
+        the_mock.should respond_to(:errors)
+      end
+      
+      describe "mock object errors stub" do
+        it "should return 0 for count" do
+          the_mock.errors.count.should == 0
+        end
+        
+        it "should return nil for on(:attribute)" do
+          the_mock.errors.on(:name).should be_nil
+        end
       end
     end  
-  end
-  
+  end  
+
 end
