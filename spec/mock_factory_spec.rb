fc6d6f10bf2e3f3d94087e4b27ff6fbd69132200
Adam Meehan <adam.meehan@gmail.com>
2008-08-30 15:41:38 +1000

    added back mocha and flexmock for specs and renamed test mock

--- spec/mock_factory_spec.rb
+++ spec/mock_factory_spec.rb
@@ -5,55 +5,52 @@ describe MockedFixtures::MockFactory do
     @fixture = {:id => 6, :cid => 6, :name => 'Hot Mocks'}
   end
 
-  #[:rspec, :flexmock, :mocha].each do |type|
-  [:rspec].each do |type|
+  [:rspec, :flexmock, :mocha].each do |type|
     require "mocked_fixtures/mocks/#{type}"
-    require Spec::Runner.configuration.mock_framework
-    include Spec::Plugins::MockFramework
     
-    describe "#{type.to_s.titleize} integration" do      
-      attr_accessor :the_mock
+    describe " with #{type.to_s.titleize} integration" do
+      attr_accessor :a_mock
       
       before do
-        @the_mock = MockedFixtures::MockFactory.create_mock(type, Company, @fixture, self)        
+        @a_mock = MockedFixtures::MockFactory.create_mock(type, Company, @fixture, self)        
       end
       
       it "should create mock fixture object" do        
-        the_mock.should_not be_nil
+        a_mock.should_not be_nil
       end
       
       it "should create mock object with all attributes" do        
-        the_mock.should respond_to(:cid, :name, :address, :created_at, :updated_at)
+        a_mock.should respond_to(:cid, :name, :address, :created_at, :updated_at)
       end
       
       it "should create mock object with attributes value from fixture" do
-        the_mock.cid.should == 6
-        the_mock.name.should == 'Hot Mocks'
+        a_mock.cid.should == 6
+        a_mock.name.should == 'Hot Mocks'
       end
       
       it "should create mock with id stubbed to fixture" do
-        the_mock.id.should == 6
+        a_mock.id.should == 6
       end
       
       it "should create mock with new_record? stubbed to false" do
-        the_mock.new_record?.should be_false        
+        a_mock.new_record?.should be_false
       end
       
       it "should create mock with to_param stubbed to id as string" do
-        the_mock.to_param.should == "6"
+        a_mock.to_param.should == "6"
       end
       
       it "should create mock with errors stubbed" do
-        the_mock.should respond_to(:errors)
+        a_mock.should respond_to(:errors)
       end
       
       describe "mock object errors stub" do
         it "should return 0 for count" do
-          the_mock.errors.count.should == 0
+          a_mock.errors.count.should == 0
         end
         
         it "should return nil for on(:attribute)" do
-          the_mock.errors.on(:name).should be_nil
+          a_mock.errors.on(:name).should be_nil
         end
       end
     end  
