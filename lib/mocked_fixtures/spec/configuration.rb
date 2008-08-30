c12ef1961fd9d4833bf885d5ddad3470919bf091
Adam Meehan <adam.meehan@gmail.com>
2008-08-30 15:58:54 +1000

    updated to rspec style mock_with method without '='

--- lib/mocked_fixtures/spec/configuration.rb
+++ lib/mocked_fixtures/spec/configuration.rb
@@ -9,8 +9,8 @@ module MockedFixtures
         Test::Unit::TestCase.mock_fixtures(*fixtures)
       end
       
-      def mock_fixtures_with=(mock_framework)
-        Test::Unit::TestCase.mock_fixtures_with = mock_framework
+      def mock_fixtures_with(mock_framework)
+        Test::Unit::TestCase.mock_fixtures_with mock_framework
       end
     end
   end
