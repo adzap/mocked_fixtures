22efe07f8032f86fe8a5bbb9572eefd444209590
Adam Meehan <adam.meehan@gmail.com>
2008-08-30 15:42:30 +1000

    removed mocking libraries which are required as needed

--- lib/mocked_fixtures.rb
+++ lib/mocked_fixtures.rb
@@ -9,6 +9,7 @@ require 'mocked_fixtures/version'
 
 if defined?(Spec::Example)
   require 'mocked_fixtures/spec/configuration'
+  Test::Unit::TestCase.mocked_fixtures_mock_framework = :rspec
 end
 
 MockedFixtures::SchemaParser.schema_path = RAILS_ROOT + '/db/schema.rb'
