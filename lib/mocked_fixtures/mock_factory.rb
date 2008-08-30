bda9250374aa0380ff6c3c1ce8f84df8b2a544a2
Adam Meehan <adam.meehan@gmail.com>
2008-08-30 15:44:41 +1000

    simplified factory may remove altogther later

--- lib/mocked_fixtures/mock_factory.rb
+++ lib/mocked_fixtures/mock_factory.rb
@@ -4,20 +4,9 @@ module MockedFixtures
       
       # Create mock object from fixture
       def create_mock(type, model_class, fixture, testcase)
-        self.send("create_#{type}_mock", model_class, fixture, testcase)
+        testcase.send("mock_model_with_#{type}", model_class, { :all_attributes => true, :add_errors => true }.merge(fixture) )
       end
-      
-      def create_rspec_mock(model_class, fixture, testcase)
-        testcase.mock_model(model_class, { :all_attributes => true, :add_errors => true }.merge(fixture) )
-      end
-      
-      def create_flexmock_mock(model_class, fixture, testcase)
-        testcase.flexmock(:model, model_class, { :all_attributes => true, :add_errors => true }.merge(fixture) )
-      end
-      
-      def create_mocha_mock(model_class, fixture, testcase)
-        testcase.stub(model_class, { :all_attributes => true, :add_errors => true }.merge(fixture) )
-      end      
+
     end
   end
 end
