84d32ca9bf014474e00d1c02edae13b77c3f10e7
Adam Meehan <adam.meehan@gmail.com>
2008-09-04 16:41:14 +1000

    moved alot of setup into spec_helper and cleaned up to use spec helper connect_db to change connections

--- spec/connection_adapters/sqlserver_adapter_spec.rb
+++ spec/connection_adapters/sqlserver_adapter_spec.rb
@@ -2,33 +2,18 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 begin
   gem 'activerecord-sqlserver-adapter'
+  require 'active_record/connection_adapters/sqlserver_adapter'
   require 'mocked_fixtures/connection_adapters/sqlserver_adapter'
-rescue LoadError
+rescue LoadError, StandardError
   puts 'Testing ActiveRecord SQLServer Adapter skipped'
 end
 
-if defined?(MockedFixtures::ConnectionAdapters::SQLServerAdapter)
-  conn = {
-    :adapter  => 'sqlserver',
-    :host     => 'localhost',
-    :mode     => 'odbc', 
-    :dsn      => 'activerecord_unittest',
-    :database => 'activerecord_unittest',
-    :username => 'rails',
-    :password => nil
-  }
-
-  ActiveRecord::Base.establish_connection(conn)
+if defined?(ActiveRecord::ConnectionAdapters::SQLServerAdapter)
 
   ActiveRecord::ConnectionAdapters::SQLServerAdapter.send(:include, MockedFixtures::ConnectionAdapters::SQLServerAdapter)
 
-  require 'resources/schema.rb'
-
   describe MockedFixtures::ConnectionAdapters::SQLServerAdapter do
-    
-    before do
-      ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connection.active?
-    end
+    connect_db('mssql')    
     
     it "should allow SchemDumper to dump primary key option for pk other than 'id'" do
       schema = StringIO.new
@@ -40,5 +25,7 @@ if defined?(MockedFixtures::ConnectionAdapters::SQLServerAdapter)
     it "should return primary key and sequence" do
       ActiveRecord::Base.connection.pk_and_sequence_for('companies').should == ['cid', nil]
     end
+    
+    disconnect_db
   end
 end
