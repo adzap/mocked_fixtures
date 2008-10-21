fc1ace07f6e4192f229bf3059c5b49d1573467ed
Adam Meehan <adam.meehan@gmail.com>
2008-10-21 21:38:58 +1100

    bit of refactoring and added examples to cover full mock_model behaviour

--- spec/mock_factory_spec.rb
+++ spec/mock_factory_spec.rb
@@ -8,51 +8,62 @@ describe MockedFixtures::MockFactory do
   [:rspec, :flexmock, :mocha].each do |type|
     require "mocked_fixtures/mocks/#{type}"
     
-    describe " with #{type.to_s.titleize} integration" do
+    describe "with #{type.to_s.titleize} integration" do
       attr_accessor :a_mock
       
-      before do
-        @a_mock = MockedFixtures::MockFactory.create_mock(type, Company, @fixture, self)
-      end
-      
-      it "should create mock fixture object" do        
-        a_mock.should_not be_nil
-      end
-      
-      it "should create mock object with all attributes" do        
-        a_mock.should respond_to(:cid, :name, :address, :created_at, :updated_at)
-      end
-      
-      it "should create mock object with attributes value from fixture" do
-        a_mock.cid.should == 6
-        a_mock.name.should == 'Hot Mocks'
-      end
-      
-      it "should create mock with id stubbed to fixture" do
-        a_mock.id.should == 6
-      end
-      
-      it "should create mock with new_record? stubbed to false" do
-        a_mock.new_record?.should be_false
-      end
-      
-      it "should create mock with to_param stubbed to id as string" do
-        a_mock.to_param.should == "6"
-      end
-      
-      it "should create mock with errors stubbed" do
-        a_mock.should respond_to(:errors)
-      end
-      
-      describe "mock object errors stub" do
-        it "should return 0 for count" do
-          a_mock.errors.count.should == 0
+      describe "the mocked fixture object created" do
+        before do
+          @a_mock = MockedFixtures::MockFactory.create_mock(type, Company, @fixture, self)
         end
         
-        it "should return nil for on(:attribute)" do
-          a_mock.errors.on(:name).should be_nil
+        it "should have all attributes stubbed" do
+          a_mock.should respond_to(:cid, :name, :address, :created_at, :updated_at)
+        end
+
+        it "should have attribute values from fixture" do
+          a_mock.cid.should == 6
+          a_mock.name.should == 'Hot Mocks'
+        end
+
+        it "should have id stubbed to fixture id value" do
+          a_mock.id.should == 6
+        end
+
+        it "should have new_record? stubbed to false" do
+          a_mock.new_record?.should be_false
+        end
+        
+        it "should have to_param stubbed to id as string" do
+          a_mock.to_param.should == "6"
+        end
+        
+        it "should have errors stubbed" do
+          a_mock.should respond_to(:errors)
+        end
+        
+        describe "mock object errors stub" do
+          it "should return 0 for count" do
+            a_mock.errors.count.should == 0
+          end
+          
+          it "should return nil for on(:attribute)" do
+            a_mock.errors.on(:name).should be_nil
+          end
+        end
+        
+        it "should say it is a kind of model class" do
+          a_mock.kind_of?(Company).should be_true
+        end
+        
+        it "should say it is an instance of the model class" do
+          a_mock.instance_of?(Company).should be_true
+        end
+        
+        it "should return the model class as its class" do
+          a_mock.class.should == Company
         end
       end
+      
     end  
   end  
 
